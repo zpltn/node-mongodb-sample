f2fabdfe7d6f54d1512fd77ffb9f5cb37675e576
zpltn <zaplotny.gautier@web.de>
2021-04-07 15:06:06 +0200

    server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json({"message": "Welcome to the Würth IT + ITensis, Nodejs + MongoDB Stack test."});
 });
 
 require('./app/routes/note.routes.js')(app);
